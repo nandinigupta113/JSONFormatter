ee5623ecbc5b7c76dfea9254284f9b8455211022
Nandini Gupta <nandanigupta1132003@gmail.com>
2022-07-23 17:45:26 +0530

    functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import "./App.css";
 import * as React from "react";
+import { useState,useRef } from "react";
 import Button from "@mui/material/Button";
 function App() {
+  const [content, setContent] = useState("");
+  const [content2, setContent2] = useState("");
+  const ref = useRef();
+  const handleClear = () => {
+    setContent("");
+    setContent2("");
+ }
+
+ const handleformat = () => {
+  var json = JSON.parse(ref.current.value);
+    setContent2(JSON.stringify(json,null,1));
+  }
   return (
     <div className="App">
       <div className="block1">
-      <Button variant="contained" color="success">
+      <Button onClick={handleformat} variant="contained" color="success">
         Format JSON
       </Button>
-      <Button variant="contained" color="error">
+      <Button onClick={handleClear} variant="contained" color="error">
         Clear Data
       </Button>
       </div>
       <div className="block2">
         <div className="box">
           <div className="head">Enter JSON Data</div>
-          <textarea></textarea>
+          <textarea value={content} ref={ref} onChange={(e) => {
+            setContent(e.target.value)}}></textarea>
         </div>
         <div className="box">
           <div className="head">Formatted JSON Data</div>
-          <textarea></textarea>
+          <textarea value={content2} readOnly></textarea>
         </div>
       </div>
     </div>
